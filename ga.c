9984c78a882fbf78e829b0e50ba039384a63b2cc
Tássio Noronha <tassionoronha@gmail.com>
2014-11-30 22:42:15 -0300

    Indentações e otimização

--- ga.c
+++ ga.c
@@ -17,19 +17,19 @@ void imprime_p(populacao *p);
 
 int
 main(){
-    int i=0;
-    populacao *p=cria_populacao(20);
+    int i = 0;
+    populacao *p = cria_populacao(20);
     imprime_p(p);
 }
 
 
 
-individuo *cria_individuo(){
+individuo *cria_individuo(){ // Função cria o individuo gerando os genes aleatórios
     int i=0;
-    individuo *n=(individuo*)malloc(sizeof(individuo));
+    individuo *n = (individuo*)malloc(sizeof(individuo));
     
     while(i<5){
-        n->genotipo[i]=rand()%2;
+        n->genotipo[i] = rand()%2;
         i++;
     }
     
@@ -38,27 +38,27 @@ individuo *cria_individuo(){
 }
 
 populacao *cria_populacao(int n){  // tem como parametro a quantidade de individuos da população
-    int i=0;
+    int i=1;
     srand( (unsigned)time(NULL) );
     individuo *aux;
-    populacao *p=(populacao*)malloc(sizeof(populacao));
-    aux=cria_individuo();
-    p->inicio=aux;
+    populacao *p = (populacao*)malloc(sizeof(populacao));
+    aux = cria_individuo();
+    p->inicio = aux;
     
-    while(i<n-1){
-       	aux->prox=cria_individuo();
-       	aux=aux->prox;
+    while(i<n){
+       	aux->prox = cria_individuo();
+       	aux = aux->prox;
        	i++;
     }
     
     return p;
 }
 
-void imprime_p(populacao *p){// função que imprime população
-    individuo *i=p->inicio;
+void imprime_p(populacao *p){ // função que imprime população
+    individuo *i = p->inicio;
     int t=0;
     
-    while(i->prox!=NULL){
+    while(i->prox != NULL){
         int f=0;
         while(f<5){
             printf("%d ",i->genotipo[f]);
